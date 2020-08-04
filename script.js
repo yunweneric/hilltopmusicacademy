90ba19fe8020f85f0f742780a80e6acd9b6929ac
yunweneric <yunweneric@mail.com>
2020-08-04 13:15:16 +0100

    added preloader

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+// $(document).ready(function () {
+//     window.onload = function () {
+//         $("preload").fadeout(500);
+//         $('.preload').hide();
+//     };
+//     $('#maincontent').show();
+// alert("hello")
+
+//     }
+// );
 $(document).ready(function () {
     window.onload = function () {
-        $('.preload').fadeOut(500, function () { $('.preload').remove(); });
-        $('.maincontent').fadeOut(500, function () { $('.preload').show(); });
+        $('.preload').fadeOut(500, function () { $('.preload').hide(); });
+        $('#maincontent').fadeIn(500, function () { $('#maincontent').show(); });
+            // $('#maincontent').show();
 
     }
 });
